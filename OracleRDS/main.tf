df3f208be000c76ba014cfcac7489a17fa7874f6
Swapnil Barsagade <94060218+Swapnilbarsagade@users.noreply.github.com>
2024-11-07 14:28:18 +0530

    qwertt

--- OracleRDS/main.tf
+++ OracleRDS/main.tf
@@ -9,7 +9,7 @@ resource "aws_db_instance" "oracle_db" {
   allocated_storage       = var.db_allocated_storage
   engine                  = "oracle-se2"       # Or "oracle-se1", "oracle-ee" depending on your needs
   instance_class          = var.db_instance_class
-  name                    = var.db_name
+  db_name                    = var.db_name
   username                = var.db_username
   password                = var.db_password
   parameter_group_name    = var.db_parameter_group
