4cc6bf08d590a3e59942e7784d1bb4eda6d8bd69
Swapnil Barsagade <94060218+Swapnilbarsagade@users.noreply.github.com>
2024-11-08 17:20:28 +0530

    qwerty

--- VPC-Peering/main.tf
+++ VPC-Peering/main.tf
@@ -1,52 +1,166 @@
-# main.tf
 
-# AWS provider configuration
 provider "aws" {
+  region  = var.aws_region
   profile = var.profile
-  region  = var.region
 }
 
-# Create the first VPC
-resource "aws_vpc" "vpc_a" {
-  cidr_block           = var.vpc_a_cidr
+# First VPC
+resource "aws_vpc" "vpc_1" {
+  cidr_block           = var.vpc_cidr_1
   enable_dns_support   = true
   enable_dns_hostnames = true
   tags = {
-    Name = var.vpc_a_name
+    Name = "VPC-1"
   }
 }
 
-# Create the second VPC
-resource "aws_vpc" "vpc_b" {
-  cidr_block           = var.vpc_b_cidr
+# Second VPC
+resource "aws_vpc" "vpc_2" {
+  cidr_block           = var.vpc_cidr_2
   enable_dns_support   = true
   enable_dns_hostnames = true
   tags = {
-    Name = var.vpc_b_name
+    Name = "VPC-2"
   }
 }
 
-# VPC Peering Connection
-resource "aws_vpc_peering_connection" "peer_ab" {
-  vpc_id        = aws_vpc.vpc_a.id
-  peer_vpc_id   = aws_vpc.vpc_b.id
-  peer_region   = var.region
-
+# Subnets
+resource "aws_subnet" "subnet_1" {
+  vpc_id            = aws_vpc.vpc_1.id
+  cidr_block        = var.subnet_cidr_1
+  map_public_ip_on_launch = true
   tags = {
-    Name = "${var.vpc_a_name}-to-${var.vpc_b_name}-peering"
+    Name = "Subnet-1"
   }
 }
 
-# Route for VPC-A to VPC-B
-resource "aws_route" "vpc_a_to_vpc_b" {
-  route_table_id         = aws_vpc.vpc_a.main_route_table_id
-  destination_cidr_block = var.vpc_b_cidr
-  vpc_peering_connection_id = aws_vpc_peering_connection.peer_ab.id
+resource "aws_subnet" "subnet_2" {
+  vpc_id            = aws_vpc.vpc_2.id
+  cidr_block        = var.subnet_cidr_2
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "Subnet-2"
+  }
 }
 
-# Route for VPC-B to VPC-A
-resource "aws_route" "vpc_b_to_vpc_a" {
-  route_table_id         = aws_vpc.vpc_b.main_route_table_id
-  destination_cidr_block = var.vpc_a_cidr
-  vpc_peering_connection_id = aws_vpc_peering_connection.peer_ab.id
+# Internet Gateways
+resource "aws_internet_gateway" "igw_1" {
+  vpc_id = aws_vpc.vpc_1.id
+  tags = {
+    Name = "IGW-1"
+  }
+}
+
+resource "aws_internet_gateway" "igw_2" {
+  vpc_id = aws_vpc.vpc_2.id
+  tags = {
+    Name = "IGW-2"
+  }
+}
+
+# Route Tables
+resource "aws_route_table" "rt_1" {
+  vpc_id = aws_vpc.vpc_1.id
+}
+
+resource "aws_route_table" "rt_2" {
+  vpc_id = aws_vpc.vpc_2.id
+}
+
+# Routes for Internet Access
+resource "aws_route" "route_igw_1" {
+  route_table_id         = aws_route_table.rt_1.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw_1.id
+}
+
+resource "aws_route" "route_igw_2" {
+  route_table_id         = aws_route_table.rt_2.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw_2.id
+}
+
+# Associate Route Tables with Subnets
+resource "aws_route_table_association" "rta_1" {
+  subnet_id      = aws_subnet.subnet_1.id
+  route_table_id = aws_route_table.rt_1.id
+}
+
+resource "aws_route_table_association" "rta_2" {
+  subnet_id      = aws_subnet.subnet_2.id
+  route_table_id = aws_route_table.rt_2.id
+}
+
+# VPC Peering
+resource "aws_vpc_peering_connection" "vpc_peering" {
+  vpc_id        = aws_vpc.vpc_1.id
+  peer_vpc_id   = aws_vpc.vpc_2.id
+  auto_accept   = true
+}
+
+# Routes for VPC Peering
+resource "aws_route" "route_vpc_peering_1" {
+  route_table_id         = aws_route_table.rt_1.id
+  destination_cidr_block = var.vpc_cidr_2
+  vpc_peering_connection_id = aws_vpc_peering_connection.vpc_peering.id
+}
+
+resource "aws_route" "route_vpc_peering_2" {
+  route_table_id         = aws_route_table.rt_2.id
+  destination_cidr_block = var.vpc_cidr_1
+  vpc_peering_connection_id = aws_vpc_peering_connection.vpc_peering.id
+}
+
+# Security Groups
+resource "aws_security_group" "sg_ping" {
+  vpc_id = aws_vpc.vpc_1.id
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [aws_vpc.vpc_2.cidr_block]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "sg_ping_2" {
+  vpc_id = aws_vpc.vpc_2.id
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [aws_vpc.vpc_1.cidr_block]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# EC2 Instances in Each VPC
+resource "aws_instance" "instance_1" {
+  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI
+  instance_type = var.instance_type
+  subnet_id     = aws_subnet.subnet_1.id
+  security_groups = [aws_security_group.sg_ping.name]
+  tags = {
+    Name = "Instance-1"
+  }
+}
+
+resource "aws_instance" "instance_2" {
+  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI
+  instance_type = var.instance_type
+  subnet_id     = aws_subnet.subnet_2.id
+  security_groups = [aws_security_group.sg_ping_2.name]
+  tags = {
+    Name = "Instance-2"
+  }
 }
--- VPC-Peering/variable.tf
+++ VPC-Peering/variable.tf
@@ -1,30 +1,34 @@
-# variables.tf
+
+variable "aws_region" {
+  default = "ap-northeast-2"
+}
+
 variable "profile" {
   description = "AWS CLI profile to use"
   default     = "swapnil"
 }
 
-variable "region" {
-  description = "AWS region to deploy resources"
-  default     = "ap-northeast-2"
-}
-
-variable "vpc_a_cidr" {
+variable "vpc_cidr_1" {
   description = "CIDR block for the first VPC"
   default     = "10.0.0.0/16"
 }
 
-variable "vpc_b_cidr" {
+variable "vpc_cidr_2" {
   description = "CIDR block for the second VPC"
   default     = "10.1.0.0/16"
 }
 
-variable "vpc_a_name" {
-  description = "Name tag for the first VPC"
-  default     = "VPC-A"
+variable "subnet_cidr_1" {
+  description = "CIDR block for the subnet in the first VPC"
+  default     = "10.0.1.0/24"
 }
 
-variable "vpc_b_name" {
-  description = "Name tag for the second VPC"
-  default     = "VPC-B"
+variable "subnet_cidr_2" {
+  description = "CIDR block for the subnet in the second VPC"
+  default     = "10.1.1.0/24"
+}
+
+variable "instance_type" {
+  description = "Instance type for the EC2 instances"
+  default     = "t2.micro"
 }
