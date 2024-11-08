58393e24205811d248fbb00aeab59c8478972c41
Swapnil Barsagade <94060218+Swapnilbarsagade@users.noreply.github.com>
2024-11-08 18:43:32 +0530

    qwerty

--- VPC-Peering/main.tf
+++ VPC-Peering/main.tf
@@ -158,25 +158,25 @@ resource "aws_security_group" "sg_ping_2" {
   }
 }
 
-# EC2 Instances in Each VPC
 resource "aws_instance" "instance_1" {
-  ami                 = "ami-03d31e4041396b53c" # Amazon Linux 2 AMI
-  instance_type       = var.instance_type
-  subnet_id           = aws_subnet.subnet_1.id
-  availability_zone = "ap-northeast-2a"  # Change this to a valid AZ
-  vpc_security_group_ids = [aws_security_group.sg_ping.id]
+  ami           = "ami-03d31e4041396b53c" # Amazon Linux 2 AMI
+  instance_type = var.instance_type
+  subnet_id     = aws_subnet.subnet_1.id
+  availability_zone = "ap-northeast-2b"  # Match the Availability Zone with the subnet
+  security_group_ids = [aws_security_group.sg_ping.id]
   tags = {
     Name = "Server-1"
   }
 }
 
 resource "aws_instance" "instance_2" {
-  ami                 = "ami-03d31e4041396b53c" # Amazon Linux 2 AMI
-  instance_type       = var.instance_type
-  subnet_id           = aws_subnet.subnet_2.id
-  availability_zone = "ap-northeast-2a"  # Change this to a valid AZ
-  vpc_security_group_ids = [aws_security_group.sg_ping_2.id]
+  ami           = "ami-03d31e4041396b53c" # Amazon Linux 2 AMI
+  instance_type = var.instance_type
+  subnet_id     = aws_subnet.subnet_2.id
+  availability_zone = "ap-northeast-2c"  # Match the Availability Zone with the subnet
+  security_group_ids = [aws_security_group.sg_ping_2.id]
   tags = {
     Name = "Server-2"
   }
 }
+
