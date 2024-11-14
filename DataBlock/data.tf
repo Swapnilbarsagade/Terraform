2e9a2bc92b391d8c846b46ba17c9123eb4434296
Swapnil Barsagade <94060218+Swapnilbarsagade@users.noreply.github.com>
2024-11-14 15:06:47 +0530

    qwerty

--- DataBlock/data.tf
+++ DataBlock/data.tf
@@ -1,12 +1,12 @@
-data "aws_ami" "this_aws_ami" {
-    name_regex       = "ami_use"
+data "aws_ami" "this_boxer_ami" {
+    name_regex       = "boxer_ami"
       filter {
     name   = "name"
-    values = ["ami_use"]
+    values = ["boxer_ami"]
     }
 
 }
 
-data "aws_security_group" "lb_sg" {
+data "aws_security_group" "default" {
   name = "default"  #var.vpc_security_group_ids[2]
 }
