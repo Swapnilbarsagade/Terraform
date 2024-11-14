71dd565b91dada96ea96bb7f862d76eea9940195
Swapnil Barsagade <94060218+Swapnilbarsagade@users.noreply.github.com>
2024-11-14 13:09:03 +0530

    qwertttty

--- Studentapp/main.tf
+++ Studentapp/main.tf
@@ -44,16 +44,32 @@ resource "aws_instance" "web_server_instance" {
   user_data = <<-EOF
               #!/bin/bash
               sudo apt update -y
-              
-              # Download and install Tomcat 9
-              wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.97/bin/apache-tomcat-9.0.97.tar.gz -P /tmp
-              sudo tar -xzvf /tmp/apache-tomcat-9.0.97.tar.gz -C /opt/
-              sudo mv /opt/apache-tomcat-9.0.97 /opt/tomcat9
-              sudo chmod +x /opt/tomcat9/bin/*.sh
-              
+              sudo apt install openjdk-11-jre-headless -y maven
+              sudo apt install mariadb-client -y
+
+              # Clone the repository
+              git clone https://github.com/Aamantamboli/Studentapp.git /home/ubuntu/Studentapp
+              # Navigate to the project directory and build it
+              cd /home/ubuntu/Studentapp
+              mvn clean package
+
+              # Download and install Tomcat
+              cd /tmp
+              wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.97/bin/apache-tomcat-9.0.97.tar.gz  #Check for new version if doesnt work
+              tar -xvf apache-tomcat-9.0.97.tar.gz
+              sudo mv apache-tomcat-9.0.97 /opt/tomcat
+
+              # Set JAVA_HOME
+              export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
+              export CATALINA_HOME=/opt/tomcat
+
+              # Copy the built artifacts to Tomcat's webapps directory
+              sudo cp /home/ubuntu/Studentapp/target/*.war /opt/tomcat/webapps/
+              sudo cp /home/ubuntu/Studentapp/*.jar /opt/tomcat/lib/
+
               # Start Tomcat
-              sudo /opt/tomcat9/bin/startup.sh
-              EOF
+              sudo bash /opt/tomcat/bin/catalina.sh start
+              EOF
 
   tags = {
     Name = "StudentApp"
