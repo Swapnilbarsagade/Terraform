f55e09e901236029454a2128fc52777cbbcc4b63
Swapnil Barsagade <94060218+Swapnilbarsagade@users.noreply.github.com>
2024-11-15 16:10:27 +0530

    qwerttty

--- EC2/main.tf
+++ EC2/main.tf
@@ -115,7 +115,96 @@ data "aws_route53_zone" "selected_zone" {
 resource "aws_route53_record" "student_dns_record" {
   zone_id = data.aws_route53_zone.selected_zone.zone_id
   name    = var.subdomain
-  type    = "A"
-  ttl     = 300
-  records = [aws_instance.web.public_ip]
+   type    = "A"
+  alias {
+    name                   = aws_lb.student_app_alb.dns_name
+    zone_id                = aws_lb.student_app_alb.zone_id
+    evaluate_target_health = true
+  }
+}
+
+# SSL certificate ACM
+
+resource "aws_acm_certificate" "student_certificate" {
+  domain_name = var.domain_name  # Replace with your domain
+  validation_method = "DNS"
+
+  tags = {
+    Name = "StudentApp-SSL-Certificate"
+  }
+}
+
+# Add DNS validation (if you use Route 53 for your domain)
+resource "aws_route53_record" "student_certificate_validation" {
+  zone_id = data.aws_route53_zone.selected_zone.zone_id
+  name    = "_acme-challenge.${var.domain_name}"
+  type    = "CNAME"
+  ttl     = 60
+  records = [aws_acm_certificate.student_certificate.domain_validation_options[0].resource_record_value]
+}
+
+
+#Application load balancer
+
+resource "aws_lb" "student_app_alb" {
+  name               = "student-app-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.this_student_sg.id]
+  subnets            = var.subnet_ids  # Replace with your subnet IDs
+
+  enable_deletion_protection = false
+  enable_cross_zone_load_balancing = true
+  idle_timeout = 60
+
+  tags = {
+    Name = "StudentApp-ALB"
+  }
+}
+
+resource "aws_lb_listener" "https_listener" {
+  load_balancer_arn = aws_lb.student_app_alb.arn
+  port              = "443"
+  protocol          = "HTTPS"
+
+  ssl_certificate {
+    certificate_arn = aws_acm_certificate.student_certificate.arn
+  }
+
+  default_action {
+    type             = "fixed-response"
+    fixed_response {
+      status_code = 200
+      content_type = "text/plain"
+      message_body = "HTTPS listener"
+    }
+  }
+}
+
+resource "aws_lb_target_group" "https_target_group" {
+  name     = "student-app-https-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 3
+    unhealthy_threshold = 3
+  }
+}
+
+resource "aws_lb_listener_rule" "https_listener_rule" {
+  listener_arn = aws_lb_listener.https_listener.arn
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.https_target_group.arn
+  }
+
+  condition {
+    field  = "host-header"
+    values = [var.domain_name]  # Use your domain name here
+  }
 }
