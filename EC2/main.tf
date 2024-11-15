4ecdd23d6d084786aaac6888e8106c1ec17d7fa5
Swapnil Barsagade <94060218+Swapnilbarsagade@users.noreply.github.com>
2024-11-15 16:25:07 +0530

    qwerty

--- EC2/main.tf
+++ EC2/main.tf
@@ -201,13 +201,15 @@ resource "aws_lb_target_group" "https_target_group" {
 
 resource "aws_lb_listener_rule" "https_listener_rule" {
   listener_arn = aws_lb_listener.https_listener.arn
+
   action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.https_target_group.arn
   }
 
   condition {
-    field  = "host-header"
-    values = [var.domain_name]  # Use your domain name here
+    host_header {
+      values = [var.domain_name]  # Use your domain name here
+    }
   }
 }
