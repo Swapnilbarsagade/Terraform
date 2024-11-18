9fae9b32ffdd7d11b6e696b1406e8c89bfde7e3a
Swapnil Barsagade <94060218+Swapnilbarsagade@users.noreply.github.com>
2024-11-18 12:06:16 +0530

    qwerty

--- ModuleBlock/Resources/EC2/main.tf
+++ ModuleBlock/Resources/EC2/main.tf
@@ -45,7 +45,7 @@ resource "aws_instance" "ec2_instance" {
   ami           = var.ami_id
   instance_type = var.instance_type
   subnet_id     = var.subnet_id
-  security_groups = [aws_security_group.ec2_sg.name]
+  security_group_ids = [aws_security_group.ec2_sg.name]
 
   tags = {
     Name = var.instance_name
