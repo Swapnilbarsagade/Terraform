26b92669a644fd5901ee84be8a6864df6914461b
Swapnil Barsagade <94060218+Swapnilbarsagade@users.noreply.github.com>
2024-11-22 00:15:00 +0530

    qwerty

--- StudentFull/Resources/EC2/main.tf
+++ StudentFull/Resources/EC2/main.tf
@@ -6,22 +6,6 @@ resource "aws_security_group" "ec2_sg" {
 
   # Inbound rules
   ingress {
-    description      = "Allow HTTP traffic"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description      = "Allow HTTPS traffic"
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-
-  ingress {
     description      = "Allow SSH access"
     from_port        = 22
     to_port          = 22
@@ -50,6 +34,40 @@ resource "aws_security_group" "ec2_sg" {
   }
 }
 
+# Security Group for ALB
+resource "aws_security_group" "alb_sg" {
+  name        = "${var.project_name}-alb-sg"
+  description = "Security group for ALB"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description      = "Allow HTTP traffic"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "Allow HTTPS traffic"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"] # Allow all outbound traffic
+  }
+
+  tags = {
+    Name = "${var.project_name}-alb-sg"
+  }
+}
+
 # EC2 Instance
 resource "aws_instance" "ubuntu_instance" {
   ami           = var.ubuntu_ami_id
@@ -70,7 +88,7 @@ resource "aws_lb" "app_lb" {
   name               = "${var.project_name}-alb"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.ec2_sg.id]
+  security_groups    = [aws_security_group.alb_sg.id]
   subnets            = var.public_subnet_ids
 
   tags = {
@@ -78,17 +96,17 @@ resource "aws_lb" "app_lb" {
   }
 }
 
-# Target Group
+# Target Group for Tomcat
 resource "aws_lb_target_group" "tg" {
   name     = "${var.project_name}-tg"
-  port     = 80
+  port     = 8080 # Tomcat default port
   protocol = "HTTP"
   vpc_id   = var.vpc_id
 
   health_check {
     interval            = 30
-    path                = "/"
-    port                = "80"
+    path                = "/student" # Endpoint for health checks
+    port                = "8080"
     protocol            = "HTTP"
     timeout             = 5
     unhealthy_threshold = 2
@@ -108,9 +126,9 @@ resource "aws_lb_listener" "listener" {
   }
 }
 
-# Attach Instance to Target Group
+# Attach EC2 Instance to Target Group
 resource "aws_lb_target_group_attachment" "tg_attachment" {
   target_group_arn = aws_lb_target_group.tg.arn
   target_id        = aws_instance.ubuntu_instance.id
-  port             = 80
+  port             = 8080
 }
