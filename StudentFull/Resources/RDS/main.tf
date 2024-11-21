156307f84fd9bdea6315161e4a157f708bbd65c6
Swapnil Barsagade <94060218+Swapnilbarsagade@users.noreply.github.com>
2024-11-22 00:21:38 +0530

    qwerty

--- StudentFull/Resources/RDS/main.tf
+++ StudentFull/Resources/RDS/main.tf
@@ -41,7 +41,7 @@ resource "aws_db_instance" "mariadb" {
   engine                  = "mariadb"
   engine_version          = var.engine_version
   instance_class          = var.instance_class
-  name                    = var.db_name
+  db_name                    = var.db_name
   username                = var.db_username
   password                = var.db_password
   db_subnet_group_name    = aws_db_subnet_group.main.name
