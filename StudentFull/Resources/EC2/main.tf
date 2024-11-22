54137f324690da2e080f4ec3362bd49db8712242
Swapnil Barsagade <94060218+Swapnilbarsagade@users.noreply.github.com>
2024-11-22 11:53:09 +0530

    qwerty

--- StudentFull/Resources/EC2/main.tf
+++ StudentFull/Resources/EC2/main.tf
@@ -114,11 +114,26 @@ resource "aws_lb_target_group" "tg" {
   }
 }
 
-# ALB Listener
+# ALB Listener for HTTP
 resource "aws_lb_listener" "listener" {
   load_balancer_arn = aws_lb.app_lb.arn
   port              = 80
   protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
+}
+
+# ALB Listener for HTTPS
+resource "aws_lb_listener" "https_listener" {
+  load_balancer_arn = aws_lb.app_lb.arn
+  port              = 443
+  protocol          = "HTTPS"
+
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = aws_acm_certificate.ssl_cert.arn
 
   default_action {
     type             = "forward"
