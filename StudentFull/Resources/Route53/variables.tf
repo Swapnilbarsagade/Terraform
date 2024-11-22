43794f3eb873ea79a241a5cc24ab1af774c5e121
Swapnil Barsagade <94060218+Swapnilbarsagade@users.noreply.github.com>
2024-11-22 15:34:05 +0530

    qwerty

--- StudentFull/Resources/Route53/variables.tf
+++ StudentFull/Resources/Route53/variables.tf
@@ -2,6 +2,11 @@ variable "domain_name" {
   description = "The domain name for the application"
 }
 
+variable "route53_zone_id" {
+  description = "The Route 53 hosted zone ID"
+  type        = string
+}
+
 variable "project_name" {}
 variable "alb_dns_name" {}
 variable "alb_zone_id" {}
