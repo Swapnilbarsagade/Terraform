587cb5dc57de11a838d735250712c4c8bff40d6b
Swapnil Barsagade <94060218+Swapnilbarsagade@users.noreply.github.com>
2024-11-23 12:36:40 +0530

    qwert

--- Provisioner/local/local.tf
+++ Provisioner/local/local.tf
@@ -6,7 +6,7 @@ provider "aws" {
  resource "aws_instance" "this_aws_instance" {
     ami = "ami-04cb1684c278156a3"
     vpc_security_group_ids = ["sg-0ee8e99c42ea4a2a3"]
-    key_name = "batmobile"
+    key_name = "swapkey"
     instance_type = "t2.micro"
      
      provisioner "file" {
