b478f91f502c928bb6ad9592a948ad40410aecae
Swapnil Barsagade <94060218+Swapnilbarsagade@users.noreply.github.com>
2024-11-23 13:10:07 +0530

    qwerty

--- Provisioner/remote/remote.tf
+++ Provisioner/remote/remote.tf
@@ -6,7 +6,7 @@ provider "aws" {
  resource "aws_instance" "this_aws_instance" {
     ami = "ami-04cb1684c278156a3"
     vpc_security_group_ids = ["sg-0ee8e99c42ea4a2a3"]
-    key_name = "batmobile"
+    key_name = "swapkey"
     instance_type = "t2.micro"
      
      provisioner "file" {
@@ -15,7 +15,7 @@ provider "aws" {
       connection {
     type     = "ssh"
     user     = "ec2-user"
-    private_key = file("${path.module}/id_rsa")
+    private_key = file("${path.module}/swapkey.pem")
     host     = "${self.public_ip}"
   } 
   }
