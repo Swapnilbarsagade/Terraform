d5d62ee7375acfacfe50e063c8e78a6f7a39c7d9
Swapnil Barsagade <94060218+Swapnilbarsagade@users.noreply.github.com>
2024-11-25 11:48:00 +0530

    qwerty

--- Provisioner/file/file.tf
+++ Provisioner/file/file.tf
@@ -5,17 +5,17 @@ provider "aws" {
     profile = "swapnil"
 }
  resource "aws_instance" "this_aws_instance" {
-    ami = "ami-0f1e61a80c7ab943e"
+    ami = "ami-040c33c6a51fd5d96"
     vpc_security_group_ids = ["sg-012e79c059dc4b579"]
     key_name = "swapkey"
     instance_type = "t2.micro"
      
      provisioner "file" {
     source      = "readme.md"
-    destination = "/home/ec2-user/readme.md"
+    destination = "/home/ubuntu/readme.md"
       connection {
     type     = "ssh"
-    user     = "ec2-user"
+    user     = "ubuntu"
     private_key = file("${path.module}/swapkey.pem")
     host     = "${self.public_ip}"
   }
